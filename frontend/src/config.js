16bcefec506c7973200a2d14d3dd48697e6e44d0
przemokoduje <145247252+przemekoduje@users.noreply.github.com>
2026-09-23 07:04:24 +0200

    Fix API port in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NODE_ENV === 'production' ? (process.env.REACT_APP_API_URL || '') : 'http://127.0.0.1:5001';
+export const API_BASE_URL = process.env.NODE_ENV === 'production' ? (process.env.REACT_APP_API_URL || '') : 'http://127.0.0.1:5000';
